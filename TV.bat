e1e0eeaedf3e845abc43becc43517a75c71aed7b
karlbaroque <45005391+karlbaroque@users.noreply.github.com>
2019-08-06 01:34:42 +0800

    Delete TV.bat

--- TV.bat
+++
@@ -1,6 +0,0 @@
-TASKKILL /F /IM TeamViewer.exe
-TASKKILL /F /IM TeamViewer_Desktop.exe
-TASKKILL /F /IM TeamViewer_Service.exe
-start "" "C:\Program Files (x86)\TeamViewer\TeamViewer.exe"
-start "" "C:\Program Files (x86)\TeamViewer\TeamViewer_Desktop.exe"
-start "" "C:\Program Files (x86)\TeamViewer\TeamViewer_Service.exe"
